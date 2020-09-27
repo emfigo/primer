891200b43befef88acaf3a4a033f92a13c2ad1eb
emfigo <emfigo@gmail.com>
2020-09-27 11:16:27 +0100

    🔩 Create payment methods table

---
+++ migrations/versions/e25e57e5acb8_create_payment_methods_table.py
@@ -0,0 +1,33 @@
+"""create payment methods table
+
+Revision ID: e25e57e5acb8
+Revises: 0493447dcfc8
+Create Date: 2020-09-27 10:55:49.402470
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = 'e25e57e5acb8'
+down_revision = '0493447dcfc8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'payment_methods',
+        sa.Column('id', postgresql.UUID, nullable=False, primary_key=True),
+        sa.Column('customer_id', postgresql.UUID, sa.ForeignKey('customers.id'), nullable=False),
+        sa.Column('details', postgresql.JSONB, nullable=False),
+        sa.Column('payment_provider_information_id', postgresql.UUID, sa.ForeignKey('payment_provider_informations.id'), nullable=False),
+        sa.Column('token', sa.String(1000), nullable=False),
+        sa.Column('created_at', postgresql.TIMESTAMP(timezone=True), nullable=False, unique=True, server_default=sa.func.now()),
+        sa.Column('updated_at', postgresql.TIMESTAMP(timezone=True), nullable=False, unique=True, server_default=sa.func.now()),
+    )
+
+
+def downgrade():
+    op.drop_table('payment_methods')
